39bdde92c1cdd6f4788e03723c741e5b83f7136f
Shawn Zhu <shawnkzhu@gmail.com>
2024-09-12 22:24:09 -0500

    Worked on question 1, 2, 6

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env zsh
 
 #SBATCH -p instruction
+#SBATCH -t 0-00:30:00
 
 # 2 CPU cores
 #SBATCH --cpus-per-task=2
 
 # A job name of FirstSlurm
-#SBATCH --job-name=FirstSlurm
+#SBATCH -J FirstSlurm
 
 # An output file called FirstSlurm.out
-#SBATCH --output="FirstSlurm.out"
+#SBATCH -o FirstSlurm.out
 
 # An error file called FirstSlurm.err
-#SBATCH --error=FirstSlurm.err
+#SBATCH -e FirstSlurm.err
 
 #Run a single command to print the hostname of the machine (compute node) running the job.
 hostname
---
+++ HW01/task6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>  // For printf
+#include <cstdlib> // For atoi
+
+int main(int argc, char *argv[]) {
+    // Get the integer that is given in the command line (Assuming it is an integer)
+    int N = std::atoi(argv[1]);
+
+    //Print out each integer in ascending order with printf
+    for (int i = 0; i <= N; i++){
+        
+    }
+}
