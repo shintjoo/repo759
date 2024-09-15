7bd80695ebd5f956a3f63a5eb40e7f2d3531ccf8
shintjoo <117604286+shintjoo@users.noreply.github.com>
2024-09-15 16:22:16 -0500

    task 6 slurm script

---
+++ HW01/task6.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -t 0-00:30:00
+
+#SBATCH --cpus-per-task=1
+
+#SBATCH -J zhuHW1T6
+
+#SBATCH -o zhuHW1T6.out
+
+#SBATCH -e zhuHW1T6.err
+
+#compile task6.cpp
+g++ task6.cpp-Wall-O3-std=c++17-o task6
+
+#run task6
+./task6 6
