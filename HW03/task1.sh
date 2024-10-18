1e3148f0251696f5d31beb476c7fd15acf2b67c6
Shawn Zhu <shawnkzhu@gmail.com>
2024-10-17 20:44:05 -0500

    Update task1.sh

--- HW03/task1.sh
+++ HW03/task1.sh
@@ -10,14 +10,14 @@
 
 output_file="zhuHW3T1.csv"
 
-# Compile the code (assuming mmul.cpp and task1.cpp are in the same directory)
-g++ task1.cpp matmul.cpp -O3 -fopenmp -o task1
+# Compile the code
+g++ task1.cpp matmul.cpp -Wall -O3 -std=c++17 -o task1 -fopenmp
 
 # Run the program for t = 1 to t = 20
-for n in {1..20}; do
+for t in {1..20}; do
   # Run the program and capture the output
-  time_taken=$(./task1 1024 $n | head -n 1)
+  time_taken=$(./task1 1024 $t | head -n 1)
 
   # Write n and the corresponding time to the output file
-  echo "$n,$time_taken" >> $output_file
+  echo "$t,$time_taken" >> $output_file
 done
