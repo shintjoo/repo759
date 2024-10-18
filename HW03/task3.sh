921eb846107e029476111efef6644a4f5df8209f
Shawn Zhu <skzhu@euler-login-1.engr.wisc.edu>
2024-10-17 22:49:10 -0500

    ran on euler

--- HW03/task3.sh
+++ HW03/task3.sh
@@ -8,17 +8,17 @@
 #SBATCH -e zhuHW3T3.err
 
 
-output_file="zhuHW3T3.csv"
+output_file="zhuHW3T3_2.csv"
 
 # Compile the code
 g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 
 # Run the program for t = 1 to t = 20
-for t in {1..10}; do
+for t in {1..20}; do
   n=$((10**6))
-  ts=$((2**t))
+  ts=$((2**6))
   # Run the program and capture the output
-  time_taken=$(./task3 $n 8 $ts | head -n 1)
+  time_taken=$(./task3 $n $t $ts | head -n 1)
 
   # Write n and the corresponding time to the output file
   echo "$t,$time_taken" >> $output_file
