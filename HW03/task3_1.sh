222567a8360f1c9f0ac3ee309fa2acb9aeb87eff
Shawn Zhu <shawnkzhu@gmail.com>
2024-10-17 23:04:08 -0500

    separated slurm scripts

---
+++ HW03/task3_1.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+#SBATCH -t 0-00:30:00
+#SBATCH --cpus-per-task=20 
+#SBATCH -J zhuHW3T3
+#SBATCH -o zhuHW3T3.out
+#SBATCH -e zhuHW3T3.err
+
+
+output_file="zhuHW3T3.csv"
+
+# Compile the code
+g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
+
+# Run the program for t = 1 to t = 20
+for t in {1..10}; do
+  n=$((10**6))
+  ts=$((2**t))
+  # Run the program and capture the output
+  time_taken=$(./task3 $n 8 $ts | head -n 1)
+
+  # Write n and the corresponding time to the output file
+  echo "$t,$time_taken" >> $output_file
+done
