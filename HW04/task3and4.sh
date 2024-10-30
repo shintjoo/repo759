faef412e715227e06c912e2498159338c91a0087
Shawn Zhu <shawnkzhu@gmail.com>
2024-10-29 20:06:37 -0500

    Update task3and4.sh

--- HW04/task3and4.sh
+++ HW04/task3and4.sh
@@ -6,9 +6,8 @@
 #SBATCH -o zhuHW4T4.out
 #SBATCH -e zhuHW4T4.err
 
-rm -f task3 task4static task4dynamic task4guided
+rm -f task4static task4dynamic task4guided
 
-g++ task3.cpp -Wall -O3 -std=c++17 -fopenmp -o task3
 g++ task4static.cpp -Wall -O3 -std=c++17 -fopenmp -o task4static
 g++ task4dynamic.cpp -Wall -O3 -std=c++17 -fopenmp -o task4dynamic
 g++ task4guided.cpp -Wall -O3 -std=c++17 -fopenmp -o task4guided
@@ -18,11 +17,10 @@ N=200    # number of particles
 T=100    # simulation end time
 
 # Clear previous results
-rm -f zhuHW4T3.txt zhuHW4T4static.txt zhuHW4T4dynamic.txt zhuHW4T4guided.txt
+rm -f zhuHW4T4static.txt zhuHW4T4dynamic.txt zhuHW4T4guided.txt
 
 # Run experiments for each scheduling policy
 for t in {1..8}; do
-    ./task3 $N $T $t >> zhuHW4T3.txt
     ./task4static $N $T $t >> zhuHW4T4static.txt
     ./task4dynamic $N $T $t >> zhuHW4T4dynamic.txt
     ./task4guided $N $T $t >> zhuHW4T4guided.txt
