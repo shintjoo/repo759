cb36a9106cb2bf795c562329510be82141b31931
Shawn Zhu <shawnkzhu@gmail.com>
2024-11-06 19:04:25 -0600

    Create task1.sh

---
+++ HW05/task1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+#SBATCH -J zhuHW5T1
+#SBATCH -p instruction
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH -t 0-00:05:00
+#SBATCH -o zhuHW5T1.out -e zhuHW5T15.err
+
+module load nvidia/cuda/11.8.0
+module load gcc/11.3.0
+
+# Compile program
+nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
+
+# Run program
+ ./task1
