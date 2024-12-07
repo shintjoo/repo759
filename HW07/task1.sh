b9d391a45601b55c78d614de40e96fac6da6eefa
Shawn Zhu <shawnkzhu@gmail.com>
2024-12-06 21:50:54 -0600

    update slurm files

--- HW07/task1.sh
+++ HW07/task1.sh
@@ -5,10 +5,12 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH -t 0-00:30:00
-#SBATCH -o zhuHW7T1_14.out -e zhuHW7T1_14.err
+#SBATCH -o zhuHW7T1.out -e zhuHW7T1.err
 
 module load nvidia/cuda/11.8.0
 module load gcc/11.3.0
+rm -f zhuHW7T1.out zhuHW7T1.err
+
 
 # Compile the program
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
--- HW07/task1_14.sh
+++ HW07/task1_14.sh
@@ -5,10 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH -t 0-00:30:00
-#SBATCH -o zhuHW7T1.out -e zhuHW7T1.err
+#SBATCH -o zhuHW7T1_14.out -e zhuHW7T1_14.err
 
 module load nvidia/cuda/11.8.0
 module load gcc/11.3.0
+
 
 # Compile the program
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
