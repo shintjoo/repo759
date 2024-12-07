6fc9abf0e4da4b4d1826eda5315c9555fd3de577
Shawn Zhu <shawnkzhu@gmail.com>
2024-12-06 22:12:20 -0600

    redo task1.cu to separate by types

--- HW07/task1.cu
+++ HW07/task1.cu
@@ -2,76 +2,127 @@
 #include <iostream>
 #include <random>
 #include <cstdlib>
-#include <ctime>
 #include "matmul.cuh"
 
-// Initialize separate Mersenne Twister generators
-std::mt19937 generator(759);
-std::uniform_real_distribution<float> dist_float(-1.0f, 1.0f);
-std::uniform_int_distribution<int> dist_int(-100, 100);
-std::uniform_real_distribution<double> dist_double(-1.0, 1.0);
+// Function to run and time matmul_1 (int)
+void runMatMul1(unsigned int n, unsigned int block_dim) {
+    // Initialize random number generator for int
+    std::mt19937 generator(759);
+    std::uniform_int_distribution<int> dist(-100, 100);
 
-// Function to fill integer matrices with random values
-void fillMatrix(int *matrix, unsigned int n) {
+    // Allocate and fill matrices
+    int *A = new int[n * n];
+    int *B = new int[n * n];
+    int *C = new int[n * n];
+
     for (unsigned int i = 0; i < n * n; ++i) {
-        matrix[i] = dist_int(generator);
+        A[i] = dist(generator);
+        B[i] = dist(generator);
     }
-}
-
-// Function to fill float matrices with random values
-void fillMatrix(float *matrix, unsigned int n) {
-    for (unsigned int i = 0; i < n * n; ++i) {
-        matrix[i] = dist_float(generator);
-    }
-}
-
-// Function to fill double matrices with random values
-void fillMatrix(double *matrix, unsigned int n) {
-    for (unsigned int i = 0; i < n * n; ++i) {
-        matrix[i] = dist_double(generator);
-    }
-}
-
-// Template function to run and time matrix multiplication
-template <typename T>
-void runMatMul(void (*matmulFunc)(const T *, const T *, T *, unsigned int, unsigned int),
-               unsigned int n, unsigned int block_dim) {
-    T *A = new T[n * n];
-    T *B = new T[n * n];
-    T *C = new T[n * n];
-
-    fillMatrix(A, n);
-    fillMatrix(B, n);
 
     // CUDA events for timing
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    // Start timing
     cudaEventRecord(start);
-
-    // Call the matrix multiplication function
-    matmulFunc(A, B, C, n, block_dim);
-
-    // Stop timing
+    matmul_1(A, B, C, n, block_dim);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
-    // Calculate time taken
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
 
-    // Output the first and last elements and time taken
+    std::cout << "Testing matmul_1 (int)\n";
     std::cout << C[0] << std::endl;
     std::cout << C[n * n - 1] << std::endl;
-    std::cout << milliseconds << " ms" << std::endl;
+    std::cout << milliseconds << " ms\n";
 
-    // Free memory
     delete[] A;
     delete[] B;
     delete[] C;
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+}
 
+// Function to run and time matmul_2 (float)
+void runMatMul2(unsigned int n, unsigned int block_dim) {
+    // Initialize random number generator for float
+    std::mt19937 generator(759);
+    std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+
+    // Allocate and fill matrices
+    float *A = new float[n * n];
+    float *B = new float[n * n];
+    float *C = new float[n * n];
+
+    for (unsigned int i = 0; i < n * n; ++i) {
+        A[i] = dist(generator);
+        B[i] = dist(generator);
+    }
+
+    // CUDA events for timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    matmul_2(A, B, C, n, block_dim);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    std::cout << "Testing matmul_2 (float)\n";
+    std::cout << C[0] << std::endl;
+    std::cout << C[n * n - 1] << std::endl;
+    std::cout << milliseconds << " ms\n";
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+}
+
+// Function to run and time matmul_3 (double)
+void runMatMul3(unsigned int n, unsigned int block_dim) {
+    // Initialize random number generator for double
+    std::mt19937 generator(759);
+    std::uniform_real_distribution<double> dist(-1.0, 1.0);
+
+    // Allocate and fill matrices
+    double *A = new double[n * n];
+    double *B = new double[n * n];
+    double *C = new double[n * n];
+
+    for (unsigned int i = 0; i < n * n; ++i) {
+        A[i] = dist(generator);
+        B[i] = dist(generator);
+    }
+
+    // CUDA events for timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    matmul_3(A, B, C, n, block_dim);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    std::cout << "Testing matmul_3 (double)\n";
+    std::cout << C[0] << std::endl;
+    std::cout << C[n * n - 1] << std::endl;
+    std::cout << milliseconds << " ms\n";
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
 }
@@ -85,14 +136,9 @@ int main(int argc, char **argv) {
     unsigned int n = std::atoi(argv[1]);
     unsigned int block_dim = std::atoi(argv[2]);
 
-    std::cout << "Testing matmul_1 (int)" << std::endl;
-    runMatMul(matmul_1, n, block_dim);
-
-    std::cout << "Testing matmul_2 (float)" << std::endl;
-    runMatMul(matmul_2, n, block_dim);
-
-    std::cout << "Testing matmul_3 (double)" << std::endl;
-    runMatMul(matmul_3, n, block_dim);
+    runMatMul1(n, block_dim);
+    runMatMul2(n, block_dim);
+    runMatMul3(n, block_dim);
 
     return 0;
 }
---
+++ HW07/task2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+#SBATCH -J zhuHW7T2
+#SBATCH -p instruction
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH -t 0-00:30:00
+#SBATCH -o zhuHW7T2.out -e zhuHW7T2.err
+
+module load nvidia/cuda/11.8.0
+module load gcc/11.3.0
+rm -f zhuHW7T2.out zhuHW7T2.err
+
+
+# Compile the program
+nvcc task2.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task2
+
+# Run task1 for n = 2^5, 2^6, ..., 2^14
+for i in {10..29}; do
+    n=$((2**i))
+
+    ./task2 $n 256 >> zhuHW7T1_256.out
+
+    ./task2 $n 1024 >> zhuHW7T1_1024.out
+done
+
+# Clean up
+rm task2
